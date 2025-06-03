ada6effbfdfb45412bf00a225545f5ab732c3a2e
Vaidas Cesonis <vaices1974@gmail.com>
2025-06-03 09:31:22 +0300

    Initial commit

---
+++ src/test/java/lt/ca/javau12/jwt/ApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.ca.javau12.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
